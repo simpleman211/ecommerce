a87e9d09bb8d4589e07f7679acee1ffd9d0d4829
simpleman211 <tuankiettruong211@gmail.com>
2024-03-17 04:47:11 +0700

    ecommerce parent push

---
+++ EcommerceWebParent/EcommerceFrontEnd/src/test/java/com/hutech/frontend/EcommerceFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutech.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
